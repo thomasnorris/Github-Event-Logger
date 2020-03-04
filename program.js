75f0ecbb086eb94c3c1e15c2838e51d60e052141
Thomas Norris <tom.norris135@gmail.com>
2020-03-04 21:50:25 +0000

    pulls stuff from the request

--- program.js
+++ program.js
@@ -1,17 +1,42 @@
 var _path = require('path');
+var _bodyParser = require('body-parser');
 var _express = require('express');
 var _app = _express();
 
 const CFG_FILE = _path.resolve(__dirname, 'config', 'config.json');
 var _cfg = readJson(CFG_FILE);
 
+_app.use(_bodyParser.json());
+
 _app.post(_cfg.express.endpoint, (req, res) => {
-    var payload = req.body
+    var payload = req.body;
+    if (!payload) {
+        // log this
+    }
+
+    var event = req.get('X-GitHub-Event');
+    var repo = payload.repository.name;
+    var branch = payload.ref.replace('refs/heads/', '');
+    logEvent(event, repo, branch)
+        .then(() => {
+            res.status(200).send();
+        })
+        .catch(() => {
+            res.status(500).send();
+        });
+    res.status(200).send();
 });
 
 _app.set('json spaces', 4);
-_app.use(_express.json());
 _app.listen(_cfg.express.port);
+
+console.log('listening on ' + _cfg.express.port);
+
+function logEvent(event, repo, branch) {
+    return new Promise((resolve, reject) => {
+
+    });
+}
 
 function readJson(filePath) {
     var fs = require('fs');
